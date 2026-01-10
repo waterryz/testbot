6471346ed5547a4967e6ba6fa44d992d536f2a0c
waterryz <alexyss.waterry@icloud.com>
2026-01-10 19:04:21 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ BOT_TOKEN = "8550400671:AAHZdPJcWi_NtkurCHGxUgmRsQKMTu3826g"
 CHANNEL_ID = -1003580316890
 SITE_URL = "https://www.primefusioncars.com/"
 
-ALLOWED_DRIVERS = {5348697217, 5470043640}
+ALLOWED_DRIVERS = {5348697217, 547004364}
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
@@ -614,6 +614,7 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
 
 
 
